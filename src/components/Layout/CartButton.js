8a2c63889930108a60b1bb7609c93af1ac70c5bf
Marko Stanojevic <82764329+greardrear@users.noreply.github.com>
2021-06-10 14:05:37 +0200

    Create CartButton.js

---
+++ src/components/Layout/CartButton.js
@@ -0,0 +1,43 @@
+import React, {useContext, useEffect, useState} from 'react'
+import classes from './CartButton.module.css'
+import CartIcon from '../Cart/CartIcon'
+import CartContext from '../../store/cart-context'
+const CartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0)
+    const { items} = cartCtx;
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+    useEffect(() => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+     const timer =   setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+    return (
+        <button className={btnClasses} onClick={props.onOpenCart}>
+            <span className={classes.icon}>
+                <CartIcon/>
+            </span>
+
+            <span> Your Cart</span>
+
+            <span className={classes.badge}>
+            {numberOfCartItems}
+            </span>
+        </button>
+    )
+}
+
+export default CartButton
